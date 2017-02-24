b9a16098fb638d4d8f788ce1f2d8bd59835ce1cf
Cecil107 <Cecil107@users.noreply.github.com>
2017-02-24 23:50:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'uLkZu8hkYgrwFRxBrbA8XYp6T',
+  consumer_secret:      '1WpRe5OncTlXYqbp9OsL2X6GVPFCUI3928SmXYLUgSuThiAu1h',
+  access_token:         '801491772434219023-mrVR7zqnRCQxFxqTanr8ofuJk9y1lSP',
+  access_token_secret:  '0ox5L2f3OPcDdKNcF22EHPrHQ2IX6hxMAuzuN3kYeg3tO'
 }
+
